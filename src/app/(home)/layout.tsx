a4687aa7a9c408060751dfc02b65e3efa0880c02
liaoyi <2417276459@qq.com>
2025-05-16 18:01:56 +0800

    feat(ui): 自定义全局 Footer :sparkles:

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,7 +2,14 @@ import type { ReactNode } from 'react'
 import { HomeLayout } from 'fumadocs-ui/layouts/home'
 import { getLinks } from 'fumadocs-ui/layouts/shared'
 import { baseOptions, linkItems } from '@/app/layout.config'
+import Footer from '@/fumadocs/components/footer'
 import { Header } from '@/fumadocs/components/header'
+import { Social } from '@/fumadocs/components/icons/social'
+
+const footerNavigation = [
+  { name: 'X', href: 'https://x.com/liaoyia', icon: Social.x },
+  { name: 'GitHub', href: 'https://github.com/liaoyio', icon: Social.github },
+]
 
 export default function Layout({ children }: { children: ReactNode }) {
   const finalLinks = getLinks(linkItems, baseOptions.githubUrl)
@@ -15,6 +22,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <div className="home-children flex flex-1 flex-col divide-y divide-dashed divide-border/70 border-border/70 border-dashed sm:border-b dark:divide-border dark:border-border">
         {children}
       </div>
+      <Footer navigation={footerNavigation} />
     </HomeLayout>
   )
 }
---
+++ src/fumadocs/components/footer.tsx
@@ -0,0 +1,38 @@
+import { QuarteredGridBackground } from '@/fumadocs/components/background'
+
+export interface FooterNavigationItem {
+  name: string
+  href: string
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
+}
+
+export default function Footer({
+  navigation,
+}: {
+  navigation: FooterNavigationItem[]
+}) {
+  return (
+    <footer>
+      <div className="relative isolate mx-auto container px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <QuarteredGridBackground maxWidthClass="container" />
+        <div className="flex justify-center gap-x-6 md:order-2">
+          {navigation.map(item => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon aria-hidden="true" className="size-6" />
+            </a>
+          ))}
+        </div>
+        <p className="mt-8 text-center text-sm/6 text-gray-600 dark:text-gray-400 md:order-1 md:mt-0">
+          &copy; 2025 liaoyi.space. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
