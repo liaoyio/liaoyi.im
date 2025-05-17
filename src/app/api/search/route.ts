f224de88f70669a9b05c3369c92d7348f6813424
liaoyi <2417276459@qq.com>
2025-05-18 01:35:42 +0800

    chore(tree): 文件结构调整 :thread:

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,4 +1,22 @@
-import { createFromSource } from 'fumadocs-core/search/server'
+import { createSearchAPI } from 'fumadocs-core/search/server'
 import { postsSource } from '@/lib/source'
 
-export const { GET } = createFromSource(postsSource)
+export const revalidate = false
+
+export const { staticGET: GET } = createSearchAPI('advanced', {
+  indexes: [
+    ...postsSource
+      .getPages()
+      .filter(page => !page.data.draft) // Filter out draft posts
+      .map((page) => {
+        return {
+          title: page.data.title,
+          description: page.data.description,
+          url: page.url,
+          id: page.url,
+          structuredData: page.data.structuredData,
+          tag: 'blog',
+        }
+      }),
+  ],
+})
