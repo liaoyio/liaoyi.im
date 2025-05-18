8d1bdf9a8a19f0a4be092607826cf27f27e664ca
liaoyi <2417276459@qq.com>
2025-05-18 14:24:14 +0800

    refactor(search): 取消静态搜索 :recycle:

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,22 +1,4 @@
-import { createSearchAPI } from 'fumadocs-core/search/server'
+import { createFromSource } from 'fumadocs-core/search/server'
 import { postsSource } from '@/lib/source'
 
-export const revalidate = false
-
-export const { staticGET: GET } = createSearchAPI('advanced', {
-  indexes: [
-    ...postsSource
-      .getPages()
-      .filter(page => !page.data.draft) // Filter out draft posts
-      .map((page) => {
-        return {
-          title: page.data.title,
-          description: page.data.description,
-          url: page.url,
-          id: page.url,
-          structuredData: page.data.structuredData,
-          tag: 'blog',
-        }
-      }),
-  ],
-})
+export const { GET } = createFromSource(postsSource)
