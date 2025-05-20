eaa204238862a27073d3c23c45d7098a8a79fde0
liaoyi <2417276459@qq.com>
2025-05-20 20:57:51 +0800

    ci(analytics): 添加 vercel analytics 和 speed-insights :bricks:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from 'react'
 import { RootProvider } from 'fumadocs-ui/provider'
+import { ProductionProvider } from '@/components/_production'
 import { geistMono, geistSans } from '@/fonts'
 import { cn } from '@/lib/cn'
 import { baseUrl, createMetadata } from '@/lib/metadata'
 import { description } from './layout.config'
+
 import '../css/style.css'
 
 export const metadata = createMetadata({
@@ -19,6 +21,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
       <body className={cn('relative flex min-h-svh flex-col overflow-x-hidden')}>
+        <ProductionProvider />
         <RootProvider search={{ options: { type: 'fetch' } }}>
           {children}
         </RootProvider>
---
+++ src/components/_production/index.tsx
@@ -0,0 +1,15 @@
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
+export function ProductionProvider() {
+  return (
+    <>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Analytics />
+          <SpeedInsights />
+        </>
+      )}
+    </>
+  )
+}
